c57bb0856ad9a3ac87fd37201fcf29e4ee6c8ecd
Malo Mignard <[malo.mignard@gmail.com]>
2022-11-02 16:28:28 +0100

    chagement de la couleur

--- agario.py
+++ agario.py
@@ -1,8 +1,3 @@
-
-# v1 : pareil mais au moins on peut sortir du programme
-# avec la touche 'q', ou avec la souris en fermant la fenêtre
-
-
 #!/usr/bin/env python
 import pygame as pg
 from random import randint
@@ -28,19 +23,20 @@ def main():
     orange = (255,128,0)
 
     position = pg.math.Vector2((taille_fen//2,taille_fen//2)) #initialisation position blop
-    rayon = 80 #rayon du blop
+    rayon = 50 #rayon du blop
     rayon_fruit = 10 #rayon du fruit
-    #rayon_mechant = 30
+    rayon_mechant = 15
 
     def position_fruit():
         return (randint(rayon_fruit,taille_fen-rayon_fruit),randint(rayon_fruit,taille_fen-rayon_fruit)) #générateur de position aléatoire
+    
     #initialisation des positions des fruits    
     pos_fruit1 = position_fruit()
     pos_fruit4 = position_fruit()
     pos_fruit3 = position_fruit()
     pos_fruit2 = position_fruit()
     
-    #pos_mechant = position_fruit()
+    pos_mechant = position_fruit()
     
     done = False
     while not done:
@@ -70,7 +66,6 @@ def main():
         #le blop suit la souris 
         direction = pg.math.Vector2.normalize(pg.math.Vector2(pg.mouse.get_pos()[0]-position[0],pg.mouse.get_pos()[1]-position[1]))
         pg.draw.circle(screen, rouge, np.add(position,direction), rayon)
-        
         position = np.add(position,direction)
 
         #manger des fruits et apparition
@@ -79,11 +74,12 @@ def main():
         pg.draw.circle(screen,rose,pos_fruit2,rayon_fruit)
         pg.draw.circle(screen,bleu,pos_fruit1,rayon_fruit)
         
-        #direction_mechant = pg.math.Vector2.normalize(pg.math.Vector2(position[0]-pos_mechant[0],position[1]-pos_mechant[1]))
-        #pg.draw.circle(screen, orange, np.add(pos_mechant,direction_mechant), rayon_mechant)
+        #déplacements du méchant
+        direction_mechant = pg.math.Vector2.normalize(pg.math.Vector2(position[0]-pos_mechant[0],position[1]-pos_mechant[1]))
+        pg.draw.circle(screen, orange, np.add(pos_mechant, direction_mechant), rayon_mechant)
+        pos_mechant = np.add(pos_mechant,direction_mechant/2)
 
-        #pos_mechant = np.add(pos_mechant,direction_mechant//2)
-
+        #manger les fruits 
         if np.sqrt((position[0] - pos_fruit1[0])**2 + (position[1] - pos_fruit1[1])**2) <= (rayon-rayon_fruit):
             pos_fruit1 = position_fruit()
             rayon+=2
@@ -100,9 +96,21 @@ def main():
             pos_fruit4 = position_fruit()
             rayon+=2
 
-        #if np.sqrt((position[0] - pos_mechant[0])**2 + (position[0] - pos_mechant[0])**2) <= (rayon-rayon_mechant):
-            #rayon = rayon//2
+        #si le méchant touche le blop et que le blop n'est pas trop gros la taille du blop est divisée par 2
+        if rayon <= 4*rayon_mechant and np.sqrt((position[0] - pos_mechant[0])**2 + (position[1] - pos_mechant[1])**2) <= (rayon):
+            rayon = rayon//2
 
+        #si la taille est 4 fois ou plus supérieure à celle du méchant, on peut le manger et le méchant réapparait plus gros
+        if rayon > 4*rayon_mechant and np.sqrt((position[0] - pos_mechant[0])**2 + (position[1] - pos_mechant[1])**2) <= (rayon):
+            rayon += 2
+            pos_mechant = position_fruit()
+            rayon_mechant += 5
+
+        #si le rayon est trop petit le jeu s'arrête
+        if rayon <= 5 :
+            done = True
+
+        #la taille du blop est indiquée par le nom de la fenetre 
         pg.display.set_caption(f'taille blop = {rayon}')
         
         
